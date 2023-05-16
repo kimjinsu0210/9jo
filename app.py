46fb8d5bd750e280f5d31fe0e29d0ee15e411689
kimjinsu <kimjinsu0210@naver.com>
2023-05-16 09:44:56 +0900

    firstProject

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:<password>@cluster0.vvgmitw.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5001, debug=True)
+
+    
