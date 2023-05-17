e5e52758664403d9397e9c790bc20a74c2672014
sumin <ysm4260@gamil.com>
2023-05-17 12:13:32 +0900

    app.py render_template 경로 수정

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('static/html/subpage.html')
 
 @app.route("/info", methods=["POST"])
 def bucket_post():
